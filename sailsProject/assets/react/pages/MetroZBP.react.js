5d3868de9fc884898fc86f35d5ac41c7d1c2f165
Alex <am3081@gmail.com>
2015-10-29 14:46:47 -0400

    sails project

---
+++ sailsProject/assets/react/pages/MetroZBP.react.js
@@ -0,0 +1,18 @@
+var React = require("react"),
+	
+	
+var Header = React.createClass({
+	render:function(){
+		
+		return (
+			<div className="container main">
+	            <h1>Hello World</h1>
+	        </div>
+
+		);
+	}
+});
+
+module.exports = Header;
+
+
