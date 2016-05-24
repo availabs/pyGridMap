cac8c972a4be012425fa3c4e94a66f69e51359d4
Larry Gloeckler <Larry@dyn-169-226-33-111.wireless.albany.edu>
2016-05-24 14:20:10 -0400

    stuff

--- sailsProject/assets/react/pages/Map.react.js
+++ sailsProject/assets/react/pages/Map.react.js
@@ -6,24 +6,30 @@ var React = require('react'),
 var MapPage = React.createClass({
 
 	getInitialState: function(){
-		return  {
+
+		return {
 			elemWidth: 800,
 			loading: false,
 			canvasData: null,
+			projection: "orthographic",
 			date: new Date(2015, 11, 31, 0),
 			format: "YYYY-MM-DD",
 			inputFormat: "MM-DD-YYYY",
 			mode: "date",
 			height: 500
 		}
+
 	},
 
-	componentDidMount:function (){
+	componentDidMount: function (){
+
 		let newDate = this.state.date
 		this.loadData(this.state.height, newDate.getFullYear(), newDate.getMonth()+1, newDate.getDate(), newDate.getHours())
+
 	},
 
 	loadData: function(height, year, month, day, hour){
+
 		var scope = this;
 		this.setState({loading: true})
 		d3.json('http://localhost:5000/grids/'+height+'/'+year+'/'+month+'/'+day+'/'+hour, function(err,data){
@@ -37,13 +43,15 @@ var MapPage = React.createClass({
 			})
 
 			scope.setState({
-				canvasData:data,
+				canvasData: data,
 				loading: false
 			})
 		})
+
 	},
 
 	loadRmmTcData: function(phase, amp, season, lat, lon, radius){
+
 		var scope = this;
 		this.setState({loading:true})
 		d3.json('http://localhost:5000/phase/'+phase+'/amp/'+amp+'/season/'+season+'/lat/'+lat+'/lon/'+lon+'/radius/'+radius, function(err,data){
@@ -58,33 +66,34 @@ var MapPage = React.createClass({
 			})
 
 			scope.setState({
-				canvasData:data,
+				canvasData: data,
 				loading: false
 			})
 		})
+
 	},
 
 	_onChange: function(dateString) {
 
 		var newDate = new Date(dateString[0])
-		console.log('test',newDate.getFullYear(), newDate.getMonth()+1, newDate.getDate())
 		let hour = newDate.getHours() % 6 === 0 ? newDate.getHours() : parseInt(newDate.getHours()/6) * 6
 		this.loadData(this.state.height, newDate.getFullYear(), newDate.getMonth()+1, newDate.getDate(), hour)
 		newDate.setHours(hour)
        	this.setState({
 			date: newDate
        	})
+
     },
 
     _dateChange: function(e) {
-    	console.log('_dateChange', e.target.value, e.target.name )
+
     	let newDate = this.state.date
-		console.log('newDate', newDate)
     	switch( e.target.name ) {
     		case 'year':
     			newDate.setFullYear(e.target.value)
     			this.loadData(this.state.height, newDate.getFullYear(), newDate.getMonth()+1, newDate.getDate(), this.state.date.getHours())
     			this.setState({date: newDate})
+				console.log('date', this.state.date)
     		break;
     		case 'month':
 				newDate.setMonth(e.target.value-1)
@@ -92,7 +101,7 @@ var MapPage = React.createClass({
     			this.setState({date: newDate})
     		break;
     		case 'day':
-				// @TODO: If not a leap year, and date is 29 January or 29 March,
+				// TODO: If not a leap year, and date is 29 January or 29 March,
 				// and want to go to 29 February, should we go to 28 Februay or 1 March?
 				// Default is 1 March.
 				newDate.setDate(e.target.value)
@@ -113,21 +122,32 @@ var MapPage = React.createClass({
     			this.setState({date: newDate})
     		break;
     	}
+
     },
 
     _heightChange(e){
 
     	let newDate = this.state.date
-    	this.setState({height:e.target.value})
+    	this.setState({height: e.target.value})
     	this.loadData(e.target.value, newDate.getFullYear(), newDate.getMonth()+1, newDate.getDate(), this.state.date.getHours())
 
     },
+
+	_projectionChange(e){
+
+		// TODO: Debug projection change utility.
+		let newDate = this.state.date
+		this.setState({projection: e.target.value})
+		this.loadData(this.state.height, newDate.getFullYear(), newDate.getMonth()+1, newDate.getDate(), this.state.date.getHours())
+		console.log('projection', this.state.projection)
+
+	},
 
 	render: function(){
 
 		const {date, format, mode, inputFormat} = this.state;
 
-		var  xScaleBrush = d3.time.scale().domain([new Date(1979, 1, 1), new Date(2015, 12, 31)]).range([0, this.state.elemWidth - 70]);
+		// var  xScaleBrush = d3.time.scale().domain([new Date(1979, 1, 1), new Date(2015, 12, 31)]).range([0, this.state.elemWidth - 70]);
 
 		let dateStyle = {
 			fontSize: 20,
@@ -141,25 +161,33 @@ var MapPage = React.createClass({
 			fontSize: 12
 		}
 
-		let secondDate = moment(this.state.date).add(30, 'days')._d
+		// let secondDate = moment(this.state.date).add(30, 'days')._d
 
 		return (
 
 			<div className="container-fluid main">
-	            A globe is gonna go here.
-	            {this.state.loading ? ' Loading.' : ' Done.'}
+	            A globe is gonna go here. Data is
+	            {this.state.loading ? ' loading.' : ' done loading.'}
 	            <br />
                 {this.state.newDate}
 	            <br />
-	            {this.state.canvasData ? this.state.canvasData.length : 'no canvasData' }
-				<div>
-
-				</div>
 	            <div>
 	            	<div className='row' style={{
 	            		boxShadow: '2px 2px 2px #5d5d5d',
 	            		backgroundColor: '#efefef', marginBottom: 15}}>
-	            		<div className='col-xs-2' />
+	            		<div className='col-xs-1'></div>
+						<div className='col-xs-2'>
+		            		<span style={labelStyle}>Projection</span>
+		            		<select style={dateStyle} onChange={this._projectionChange} name='projection' className="form-control" value={this.state.projection}>
+		            			<option value='orthographic'>Orthographic</option>
+								<option value='azimuthal_equidistant'>Azimuthal Equidistant</option>
+		            			<option value='conic_equidistant'>Conic Equidistant</option>
+								<option value='equirectangular'>Equirectangular</option>
+								<option value='stereographic'>Stereographic</option>
+								<option value='waterman'>Waterman</option>
+								<option value='winkel3'>Winkel3</option>
+		            		</select>
+		            	</div>
 		            	<div className='col-xs-2'>
 		            		<span style={labelStyle}>Level</span>
 		            		<select style={dateStyle} onChange={this._heightChange} name='height' className="form-control" value={this.state.height}>
@@ -188,33 +216,33 @@ var MapPage = React.createClass({
 			            	</div>
 			            </div>
 		            </div>
-		            <div className='row'>
-		            	<div className='col-xs-3' />
-		            	<div className='col-xs-6'>
-			            	<Brush
-			                   width={this.state.elemWidth}
-			                   height={75}
-			                   margin={{top: 0, bottom: 30, left: 50, right: 20}}
-			                   xScale={xScaleBrush}
-			                   onChange={this._onChange}
-			                   extent={[this.state.date, secondDate]}
-			                   xAxis={{tickValues: xScaleBrush.ticks(d3.time.year, 5), tickFormat: d3.time.format("%y")}} />
-				        	</div>
-			        </div>
-
                 </div>
 	            <div className='row'>
 	            	<div className='col-xs-12' style={{border: '1px solid red'}}>
-	            		<Globe canvasData={this.state.canvasData} date={date}/>
+	            		<Globe canvasData={this.state.canvasData} />
 	            	</div>
 	            </div>
-
-
 	        </div>
 
 		);
 	}
 
 });
+
+// -----------------------------BRUSH CODE--------------------------------------
+// <div className='row'>
+// 	<div className='col-xs-3' />
+// 	<div className='col-xs-6'>
+//     	<Brush
+//            width={this.state.elemWidth}
+//            height={75}
+//            margin={{top: 0, bottom: 30, left: 50, right: 20}}
+//            xScale={xScaleBrush}
+//            onChange={this._onChange}
+//            extent={[this.state.date, secondDate]}
+//            xAxis={{tickValues: xScaleBrush.ticks(d3.time.year, 5), tickFormat: d3.time.format("%y")}}
+//		/>
+//     	</div>
+// </div>
 
 module.exports = MapPage;
